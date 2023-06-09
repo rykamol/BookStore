a5307e8bb3393bc9a1ae57d25ce81af8e1a63483
Kamol Chandra Roy <kamol.roy@episerver.com>
2023-06-09 20:39:33 +0600

    configure custom login/logout path

--- src/BookStore.web/Program.cs
+++ src/BookStore.web/Program.cs
@@ -21,6 +21,17 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
+builder.Services.ConfigureApplicationCookie(option =>
+{
+	option.LoginPath = $"/Identity/Account/Login";
+	option.LogoutPath = $"/Identity/Account/Logout";
+	option.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
