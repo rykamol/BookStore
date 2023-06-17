7f47e8db280778b99b55e8bbc23ee32aca942dfe
Kamol Chandra Roy <kamol.roy@episerver.com>
2023-06-17 10:55:25 +0600

    Load summary data

--- src/BookStore.web/Areas/Customer/Controllers/CartController.cs
+++ src/BookStore.web/Areas/Customer/Controllers/CartController.cs
@@ -30,13 +30,14 @@ namespace BookStore.web.Areas.Customer.Controllers
 			{
 				ShoppingCarts = _unitOfWork.ShoppingCarts.GetAll(
 					u => u.ApplicationUserId == claim.Value,
-					includeProperties: "Product")
+					includeProperties: "Product"),
+				OrderHeader = new()
 			};
 
 			foreach (var item in ShoppingCartViewModel.ShoppingCarts)
 			{
 				item.ProductPrice = GetPriceByQuantity(item.Count, item.Product.Price, item.Product.Price50, item.Product.Price100);
-				ShoppingCartViewModel.TotalCartPrice += (item.Count * item.ProductPrice);
+				ShoppingCartViewModel.OrderHeader.OrderTotal += (item.Count * item.ProductPrice);
 			}
 
 			return View(ShoppingCartViewModel);
@@ -44,25 +45,38 @@ namespace BookStore.web.Areas.Customer.Controllers
 
 		public IActionResult Summary()
 		{
-			//var claimsIdentity = (ClaimsIdentity)User.Identity;
-			//var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
 
 
-			//ShoppingCartViewModel = new ShoppingCartViewModel
-			//{
-			//	ShoppingCarts = _unitOfWork.ShoppingCarts.GetAll(
-			//		u => u.ApplicationUserId == claim.Value,
-			//		includeProperties: "Product")
-			//};
+			ShoppingCartViewModel = new ShoppingCartViewModel
+			{
+				ShoppingCarts = _unitOfWork.ShoppingCarts.GetAll(
+					u => u.ApplicationUserId == claim.Value,
+					includeProperties: "Product"),
+				OrderHeader = new()
+			};
 
-			//foreach (var item in ShoppingCartViewModel.ShoppingCarts)
-			//{
-			//	item.ProductPrice = GetPriceByQuantity(item.Count, item.Product.Price, item.Product.Price50, item.Product.Price100);
-			//	ShoppingCartViewModel.TotalCartPrice += (item.Count * item.ProductPrice);
-			//}
+
+			ShoppingCartViewModel.OrderHeader.ApplicationUser = _unitOfWork.ApplicatiionUsers.GetFirstOrDefault(
+				u => u.Id == claim.Value);
+
+			ShoppingCartViewModel.OrderHeader.Name = ShoppingCartViewModel.OrderHeader.ApplicationUser.Name;
+			ShoppingCartViewModel.OrderHeader.PhoneNumer = ShoppingCartViewModel.OrderHeader.ApplicationUser.PhoneNumber;
+			ShoppingCartViewModel.OrderHeader.StreetAddress = ShoppingCartViewModel.OrderHeader.ApplicationUser.StreetAddress;
+			ShoppingCartViewModel.OrderHeader.City = ShoppingCartViewModel.OrderHeader.ApplicationUser.City;
+			ShoppingCartViewModel.OrderHeader.State = ShoppingCartViewModel.OrderHeader.ApplicationUser.State;
+			ShoppingCartViewModel.OrderHeader.PostalCode = ShoppingCartViewModel.OrderHeader.ApplicationUser.PostalCode;
+
+
+			foreach (var item in ShoppingCartViewModel.ShoppingCarts)
+			{
+				item.ProductPrice = GetPriceByQuantity(item.Count, item.Product.Price, item.Product.Price50, item.Product.Price100);
+				ShoppingCartViewModel.OrderHeader.OrderTotal += (item.Count * item.ProductPrice);
+			}
 
 			//return View(ShoppingCartViewModel);
-			return View();
+			return View(ShoppingCartViewModel);
 		}
 
 		public IActionResult Plus(int cartId)
--- src/Core/BookStore.Domain/ViewModels/ShoppingCartViewModel.cs
+++ src/Core/BookStore.Domain/ViewModels/ShoppingCartViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookStore.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace BookStore.Domain.ViewModels
 	{
 		public IEnumerable<ShoppingCart> ShoppingCarts { get; set; }
 
-		public double TotalCartPrice { get; set; }
+		public OrderHeader OrderHeader { get; set; }
 
 	}
 }
